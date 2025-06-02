59a997cdf6b205fcf2ef48b68cb585e40cfe7cec
Jean-Philippe <jp.ouvrard@lesoctetslibres.com>
2025-06-02 17:33:19 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-800 font-normal text-white leading-6 text-opacity-90">
+      <div className="m-0 flex min-w-[320px] max-w-(--breakpoint-xl) flex-col place-items-center justify-between p-8 text-center">
+        <h1 className="p-8 font-bold text-5xl">Organizer</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
