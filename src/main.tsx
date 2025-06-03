f13c63e8a14852ce4f6f1c3e3c0b4c10e4cbeb88
Jean-Philippe <jp.ouvrard@lesoctetslibres.com>
2025-06-03 15:16:22 +0200

    feat: add react-router

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,18 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router";
+
 import App from "./App.tsx";
 import "./index.css";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+    },
+]);
 
 const rootElement = document.getElementById("root");
 if (!rootElement) throw new Error("Failed to find the root element");
 const root = ReactDOM.createRoot(rootElement);
 
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+root.render(<RouterProvider router={router} />);
