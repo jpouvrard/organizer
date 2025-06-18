7e77bb1ea061e07b8bcc01e235fc2127ee75a8f4
Jean-Philippe <jp.ouvrard@lesoctetslibres.com>
2025-06-18 15:16:00 +0200

    feat: add app layout to router + create dashboard page

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,10 @@ import { RouterProvider, createBrowserRouter } from "react-router";
 
 import App from "@/App.tsx";
 import "@/index.css";
+import Dashboard from "@/Dashboard.tsx";
 import Home from "@/Home.tsx";
 import Login from "@/Login.tsx";
+import AppLayout from "@/layouts/AppLayout.tsx";
 import AuthLayout from "@/layouts/AuthLayout.tsx";
 import MainLayout from "@/layouts/MainLayout.tsx";
 
@@ -15,6 +17,10 @@ const router = createBrowserRouter([
             {
                 Component: MainLayout,
                 children: [{ path: "/", Component: Home }],
+            },
+            {
+                Component: AppLayout,
+                children: [{ path: "dashboard", Component: Dashboard }],
             },
             {
                 Component: AuthLayout,
