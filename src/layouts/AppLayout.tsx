15fadf1f015263fb78f90adfa3a626cd14ae3327
Jean-Philippe <jp.ouvrard@lesoctetslibres.com>
2025-06-19 10:49:56 +0200

    feat: improve AppLayout with data from children

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -3,9 +3,12 @@ import { Outlet } from "react-router";
 import Header from "@/components/Header";
 import Sidebar from "@/components/Sidebar";
 import { useSidebar } from "@/lib/context/SidebarContext";
+import { useMatches } from "react-router";
 
 export default function MainLayout() {
     const { isExpanded } = useSidebar();
+    const matches = useMatches();
+    const dataRoute = matches.pop()?.data as { title: string };
 
     return (
         <div className="min-h-screen bg-primary-foreground xl:flex">
@@ -14,7 +17,12 @@ export default function MainLayout() {
                 className={`flex-1 transition-all duration-300 ease-in-out ${isExpanded ? "lg:ml-[290px]" : "lg:ml-[90px]"}`}
             >
                 <Header />
-                <Outlet />
+                <article className="no-scrollbar h-full w-full overflow-y-auto">
+                    <header className="h-[64px]">{dataRoute?.title}</header>
+                    <section>
+                        <Outlet />
+                    </section>
+                </article>
             </main>
         </div>
     );
